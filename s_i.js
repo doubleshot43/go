fc440d686b5c50bf52d08a3edd42e553efe8ed36
doubleshot43 <32381572+doubleshot43@users.noreply.github.com>
2017-09-28 23:07:02 -0700

    Update s_i.js

--- s_i.js
+++ s_i.js
@@ -1,1 +1,15 @@
-Window.alert("message");
+function createFolder(){
+var fso,folder1;
+
+folder1='C:\\USERS\\ simeone\\a';
+
+fso=new ActiveXObject('Scripting.FileSystemObject');
+
+folderexist=false;
+if(!fso.FolderExists(folder1)){
+fso.CreateFolder(folder1);
+}
+}
+
+
+createFolder();
